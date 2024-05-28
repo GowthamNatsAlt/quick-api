0b27ea69cd9390d8aa7f85e86fb8cc4591706538
GowthamNatsAlt <gowtham.nats@gmail.com>
2024-05-28 15:07:40 +0530

    First commit

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,64 @@
+import { Redis } from "@upstash/redis/cloudflare";
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+type EnvConfig = {
+  UPSTASH_REDIS_REST_TOKEN: string;
+  UPSTASH_REDIS_REST_URL: string;
+}
+
+app.use("/*", cors());
+
+app.get("/search", async (c) => {
+  try {
+    const { UPSTASH_REDIS_REST_TOKEN, UPSTASH_REDIS_REST_URL } = env<EnvConfig>(c);
+
+    const start = performance.now();
+  
+    // ==================================
+    const redis = new Redis({
+      url: UPSTASH_REDIS_REST_URL,
+      token: UPSTASH_REDIS_REST_TOKEN
+    });
+    const query = c.req.query("q")?.toUpperCase();
+  
+    if (!query) {
+       return c.json({ error: "Missing query parameter" }, 400);
+    }
+    const res = [];
+    const rank = await redis.zrank("terms", query);
+   
+    if (rank !== null && rank !== undefined) {
+      const temp = await redis.zrange<string[]>("terms", rank, rank + 100);
+      
+      for (const el of temp) {
+        if (!el.startsWith(query)) {
+          break;
+        }
+        if (el.endsWith("*")) {
+          res.push(el.substring(0, el.length - 1));
+        }
+      }
+    }
+    // ==================================
+    
+    const end = performance.now();
+  
+    return c.json({
+      results: res,
+      duration: end - start
+    });
+  } catch (error) {
+    console.error(error); 
+    return c.json({ results: [], message: "Something went wrong." }, 500);
+  }
+});
+
+export const GET = handle(app);
+export default app as never; // This is a workaround for Vercel's edge function requirement. It should be removed when the issue is resolved. See https://github.com/honojs/hono/issues/117#issuecomment-1348649485
